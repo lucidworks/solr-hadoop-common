f49b7e1d3ad4e4961f1497f7a0bb84954d2b79dc
Cesar A <cesar.vera-bernal@lucidworks.com>
2016-02-04 20:19:26 -0500

    Add XMLInputFormat, fix DistributedCacheHandler NPE.

--- solr-hadoop-io/src/main/java/com/lucidworks/hadoop/cache/DistributedCacheHandler.java
+++ solr-hadoop-io/src/main/java/com/lucidworks/hadoop/cache/DistributedCacheHandler.java
@@ -1,8 +1,5 @@
 package com.lucidworks.hadoop.cache;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocalFileSystem;
@@ -11,33 +8,32 @@ import org.apache.hadoop.mapred.JobConf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 //This class provides methods to access the distributed cache
 @SuppressWarnings("deprecation")
 public class DistributedCacheHandler {
   private transient static Logger log = LoggerFactory.getLogger(DistributedCacheHandler.class);
 
   // Adding files to Cache
-  public static void addFileToCache(JobConf conf, Path path, String propertyName) throws Exception {
-    log.info("Adding {} to cache w/ name: {}", path, propertyName);
-    try {
-      FileSystem fileSystem = path.getFileSystem(conf);
-      if (fileSystem instanceof LocalFileSystem) {
-        FileSystem defFS = FileSystem.get(conf);
-        Path dst = new Path("/tmp/" + path.getName());
-        try {
-          defFS.mkdirs(new Path("/tmp"));// just in case
-        } catch (IOException e) {
-        }
-        defFS.copyFromLocalFile(path, dst);
-        conf.set(propertyName, dst.toUri().toString());
-        DistributedCache.addCacheFile(dst.toUri(), conf);
-      } else {
-        conf.set(propertyName, path.toUri().toString());
-        DistributedCache.addCacheFile(path.toUri(), conf);
+  public static void addFileToCache(JobConf conf, Path path, String propertyName) throws IOException {
+    log.debug("Adding {} to cache w/ name: {}", path, propertyName);
+    FileSystem fileSystem = path.getFileSystem(conf);
+    if (fileSystem instanceof LocalFileSystem) {
+      FileSystem defFS = FileSystem.get(conf);
+      Path dst = new Path("/tmp/" + path.getName());
+      try {
+        defFS.mkdirs(new Path("/tmp"));// just in case
+      } catch (IOException e) {
       }
-
-    } catch (Exception e) {
-      log.error("Error while creating distributed cache", e);
+      defFS.copyFromLocalFile(path, dst);
+      conf.set(propertyName, dst.toUri().toString());
+      DistributedCache.addCacheFile(dst.toUri(), conf);
+    } else {
+      conf.set(propertyName, path.toUri().toString());
+      DistributedCache.addCacheFile(path.toUri(), conf);
     }
   }
 
@@ -57,14 +53,16 @@ public class DistributedCacheHandler {
     BufferedReader fis = null;
     StringBuilder response = new StringBuilder();
     try {
-      for (Path p : DistributedCache.getLocalCacheFiles(conf)) {
-        String path = p.toString();
-        if (conf.get(fileName).contains(p.getName())) {
-          String line = null;
-          fis = new BufferedReader(new FileReader(p.toString()));
-          while ((line = fis.readLine()) != null) {
-            line = line.trim();
-            response.append(line).append('\n');
+      Path[] pathsInCache = DistributedCache.getLocalCacheFiles(conf);
+      if (pathsInCache != null) {
+        for (Path p : pathsInCache) {
+          if (conf.get(fileName).contains(p.getName())) {
+            String line = null;
+            fis = new BufferedReader(new FileReader(p.toString()));
+            while ((line = fis.readLine()) != null) {
+              line = line.trim();
+              response.append(line).append('\n');
+            }
           }
         }
       }
---
+++ solr-hadoop-io/src/main/java/com/lucidworks/hadoop/io/XMLInputFormat.java
@@ -0,0 +1,117 @@
+package com.lucidworks.hadoop.io;
+
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DataOutputBuffer;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapred.InputSplit;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.mapred.Reporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Reads records that are delimited by a specifc begin/end tag.
+ */
+public class XMLInputFormat extends FileInputFormat {
+
+  private transient static Logger log = LoggerFactory.getLogger(XMLInputFormat.class);
+
+  public static final String START_TAG_KEY = "lww.xml.start";
+  public static final String END_TAG_KEY = "lww.xml.end";
+
+  @Override
+  public RecordReader<Text, Text> getRecordReader(InputSplit inputSplit, JobConf jobConf, Reporter reporter) throws IOException {
+    return new XMLRecordReader((FileSplit) inputSplit, jobConf);
+  }
+
+  public static class XMLRecordReader implements RecordReader<Text, Text> {
+    private final byte[] startTag;
+    private final byte[] endTag;
+    private final long start;
+    private final long end;
+    private final FSDataInputStream fsin;
+    private final DataOutputBuffer buffer = new DataOutputBuffer();
+    private final String path;
+
+    public XMLRecordReader(FileSplit split, JobConf jobConf) throws IOException {
+      log.info("Setting up XMLRecordReader for path: " + split.getPath() + "; startTag=" + jobConf.get(START_TAG_KEY) + ", endTag=" + jobConf.get(END_TAG_KEY));
+      startTag = jobConf.get(START_TAG_KEY).getBytes("utf-8");
+      endTag = jobConf.get(END_TAG_KEY).getBytes("utf-8");
+
+      // open the file and seek to the start of the split
+      start = split.getStart();
+      end = start + split.getLength();
+      Path file = split.getPath();
+      FileSystem fs = file.getFileSystem(jobConf);
+
+      path = split.getPath().getName();
+
+      fsin = fs.open(split.getPath());
+      fsin.seek(start);
+    }
+
+    public boolean next(Text key, Text value) throws IOException {
+      if (fsin.getPos() < end) {
+        if (readUntilMatch(startTag, false)) {
+          try {
+            buffer.write(startTag);
+            if (readUntilMatch(endTag, true)) {
+              key.set(path + "-" + fsin.getPos());
+              value.set(buffer.getData(), 0, buffer.getLength());
+              return true;
+            }
+          } finally {
+            buffer.reset();
+          }
+        }
+      }
+      return false;
+    }
+
+    public Text createKey() {
+      return new Text();
+    }
+
+    public Text createValue() {
+      return new Text();
+    }
+
+    public long getPos() throws IOException {
+      return fsin.getPos();
+    }
+
+    public void close() throws IOException {
+      fsin.close();
+    }
+
+    public float getProgress() throws IOException {
+      return (fsin.getPos() - start) / (float) (end - start);
+    }
+
+    private boolean readUntilMatch(byte[] match, boolean withinBlock) throws IOException {
+      int i = 0;
+      while (true) {
+        int b = fsin.read();
+        // end of file:
+        if (b == -1) return false;
+        // save to buffer:
+        if (withinBlock) buffer.write(b);
+
+        // check if we're matching:
+        if (b == match[i]) {
+          i++;
+          if (i >= match.length) return true;
+        } else i = 0;
+        // see if we've passed the stop point:
+        if (!withinBlock && i == 0 && fsin.getPos() >= end) return false;
+      }
+    }
+  }
+}
